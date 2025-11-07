6f19133f46c9733ebdba867af9911a354ead33ec
Gonzalo Lopez <gonzalo.october@gmail.com>
2025-11-07 01:51:09 -0300

    update db config

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # tp-disenio-2025-grupo-28
+
+
+## Instalar Maven (mvn)
+
+[Instalar Maven en WIndows 10/11](https://www.youtube.com/watch?v=B5dn6DL6_Qk)
+
+### Docs
+
+ https://start.spring.io/
+
+
+### start the project
+
+> mvn spring-boot:run
+
--- src/main/java/tp/tp_disenio_2025_grupo_28/TpDisenio2025Grupo28Application.java
+++ src/main/java/tp/tp_disenio_2025_grupo_28/TpDisenio2025Grupo28Application.java
@@ -1,5 +1,7 @@
 package tp.tp_disenio_2025_grupo_28;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TpDisenio2025Grupo28Application {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load(); // carga el .env
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		
 		SpringApplication.run(TpDisenio2025Grupo28Application.class, args);
 	}
 
