102a44d4c9f7df72ccbf35d3abe845c7bbe6ab98
Gonzalo Lopez <gonzalo.october@gmail.com>
2025-11-06 23:28:53 -0300

    init project

---
+++ src/main/java/tp/tp_disenio_2025_grupo_28/TpDisenio2025Grupo28Application.java
@@ -0,0 +1,13 @@
+package tp.tp_disenio_2025_grupo_28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpDisenio2025Grupo28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpDisenio2025Grupo28Application.class, args);
+	}
+
+}
