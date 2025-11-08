674608417a681b77c1c9d63111cf7ff8f3e5d712
Gonzalo Lopez <gonzalo.lopez@doc24.com.ar>
2025-11-07 21:35:42 -0300

    update huesped model

--- src/main/java/tp/tp_disenio_2025_grupo_28/model/Huesped.java
+++ src/main/java/tp/tp_disenio_2025_grupo_28/model/Huesped.java
@@ -8,51 +8,61 @@ import java.util.Date;
 public class Huesped {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private String cuit; // PK y FK al mismo tiempo
 
-    private String nombre;
-    private String apellido;
-    private String tipoDocumento;
-    private String documento;
-    private String cuit;
+    @OneToOne
+    @MapsId // Usa el mismo ID que la entidad relacionada
+    @JoinColumn(name = "cuit") // FK a persona_fisica.cuit
+    private PersonaFisica personaFisica;
+
     private String posicionFrenteAlIva;
-    private Date fechaNacimiento;
     private Integer telefono;
     private String email;
     private String ocupacion;
 
-    // Getters y setters
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
+	public String getPosicionFrenteAlIva() {
+		return this.posicionFrenteAlIva;
+	}
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+	public void setPosicionFrenteAlIva(String posicionFrenteAlIva) {
+		this.posicionFrenteAlIva = posicionFrenteAlIva;
+	}
 
-    public String getApellido() { return apellido; }
-    public void setApellido(String apellido) { this.apellido = apellido; }
+	public Integer getTelefono() {
+		return this.telefono;
+	}
 
-    public String getTipoDocumento() { return tipoDocumento; }
-    public void setTipoDocumento(String tipoDocumento) { this.tipoDocumento = tipoDocumento; }
+	public void setTelefono(Integer telefono) {
+		this.telefono = telefono;
+	}
 
-    public String getDocumento() { return documento; }
-    public void setDocumento(String documento) { this.documento = documento; }
+	public String getEmail() {
+		return this.email;
+	}
 
-    public String getCuit() { return cuit; }
-    public void setCuit(String cuit) { this.cuit = cuit; }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getPosicionFrenteAlIva() { return posicionFrenteAlIva; }
-    public void setPosicionFrenteAlIva(String posicionFrenteAlIva) { this.posicionFrenteAlIva = posicionFrenteAlIva; }
+	public String getOcupacion() {
+		return this.ocupacion;
+	}
 
-    public Date getFechaNacimiento() { return fechaNacimiento; }
-    public void setFechaNacimiento(Date fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
+	public void setOcupacion(String ocupacion) {
+		this.ocupacion = ocupacion;
+	}
 
-    public Integer getTelefono() { return telefono; }
-    public void setTelefono(Integer telefono) { this.telefono = telefono; }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public Huesped() {}
 
-    public String getOcupacion() { return ocupacion; }
-    public void setOcupacion(String ocupacion) { this.ocupacion = ocupacion; }
+    public Huesped(PersonaFisica personaFisica, String posicionFrenteAlIva, Integer telefono, String email, String ocupacion) {
+        this.personaFisica = personaFisica;
+        this.cuit = personaFisica.getCuit();
+        this.posicionFrenteAlIva = posicionFrenteAlIva;
+        this.telefono = telefono;
+        this.email = email;
+        this.ocupacion = ocupacion;
+    }
+
+    
 }
