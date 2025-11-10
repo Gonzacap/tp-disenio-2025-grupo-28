0f655f83c33bb325e7e007f4f3b7bab05b17736b
Gonzalo Lopez <gonzalo.october@gmail.com>
2025-11-10 01:42:02 -0300

    update HuespedController

--- src/main/java/tp/tp_disenio_2025_grupo_28/controller/HuespedController.java
+++ src/main/java/tp/tp_disenio_2025_grupo_28/controller/HuespedController.java
@@ -4,13 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 import tp.tp_disenio_2025_grupo_28.model.Huesped;
 import tp.tp_disenio_2025_grupo_28.repository.HuespedRepository;
