fd1627f65293ff171a78a4c0c5e31da2b11e67de
Gonzalo Lopez <gonzalo.lopez@doc24.com.ar>
2025-11-26 14:06:49 -0300

    update habitacion web controller

--- src/main/java/tp/tp_disenio_2025_grupo_28/controller/HabitacionWebController.java
+++ src/main/java/tp/tp_disenio_2025_grupo_28/controller/HabitacionWebController.java
@@ -1,20 +1,25 @@
 package tp.tp_disenio_2025_grupo_28.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
-import org.springframework.beans.factory.annotation.*;
-// import org.springframework.dao.DuplicateKeyException;
-// import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.*;
-
-// import tp.tp_disenio_2025_grupo_28.model.*;
-import tp.tp_disenio_2025_grupo_28.service.*;
-import tp.tp_disenio_2025_grupo_28.dto.*;
+import tp.tp_disenio_2025_grupo_28.dto.HabitacionDTO;
+import tp.tp_disenio_2025_grupo_28.dto.ReservaDTO;
+import tp.tp_disenio_2025_grupo_28.model.enums.TipoHabitacion;
+import tp.tp_disenio_2025_grupo_28.service.GestionHabitacion;
 // import tp.tp_disenio_2025_grupo_28.mapper.*;
 
 @Controller
@@ -52,6 +57,9 @@ public class HabitacionWebController {
 
             List<HabitacionDTO> habitaciones = gestionHabitacion.obtenerHabitacionesOrdenadas();
 
+            Map<TipoHabitacion, List<HabitacionDTO>> habitacionesPorTipo
+                    = habitaciones.stream().collect(Collectors.groupingBy(HabitacionDTO::getTipo));
+
             List<ReservaDTO> reservas = gestionHabitacion.obtenerReservasEntre(fechaDesde, fechaHasta);
 
             List<Date> dias = gestionHabitacion.generarDiasEntre(fechaDesde, fechaHasta);
@@ -59,7 +67,8 @@ public class HabitacionWebController {
             List<Map<String, Object>> diasConEstados = gestionHabitacion.construirGrillaEstados(habitaciones, reservas, dias);
 
             model.addAttribute("tiposHabitacion", gestionHabitacion.obtenerTiposHabitacion());
-            model.addAttribute("habitaciones", habitaciones);
+            // model.addAttribute("habitaciones", habitaciones);
+            model.addAttribute("habitacionesPorTipo", habitacionesPorTipo);
             model.addAttribute("dias", diasConEstados);
 
             return "habitacion/GestionHabitacion";
