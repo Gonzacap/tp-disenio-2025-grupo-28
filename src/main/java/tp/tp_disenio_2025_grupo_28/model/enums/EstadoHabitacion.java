726349608233e5bf9d958391918929e8559d2f8c
Gonzalo Lopez <gonzalo.lopez@doc24.com.ar>
2025-11-26 14:18:46 -0300

    update enums

--- src/main/java/tp/tp_disenio_2025_grupo_28/model/enums/EstadoHabitacion.java
+++ src/main/java/tp/tp_disenio_2025_grupo_28/model/enums/EstadoHabitacion.java
@@ -1,8 +1,19 @@
 package tp.tp_disenio_2025_grupo_28.model.enums;
 
 public enum EstadoHabitacion {
-    disponible,
-    ocupada,
-    reservada,
-    mantenimiento
+
+    disponible("Disponible"),
+    ocupada("Ocupada"),
+    reservada("Reservada"),
+    mantenimiento("Mantenimiento");
+
+    private final String nombre;
+
+    EstadoHabitacion(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
