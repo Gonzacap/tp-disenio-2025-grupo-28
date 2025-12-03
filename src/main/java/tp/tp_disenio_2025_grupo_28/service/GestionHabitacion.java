465e2b2200e48270e5d67d97997578413f850e04
Gonzalo Lopez <gonzalo.lopez@doc24.com.ar>
2025-12-03 13:57:09 -0300

    fix issue

--- src/main/java/tp/tp_disenio_2025_grupo_28/service/GestionHabitacion.java
+++ src/main/java/tp/tp_disenio_2025_grupo_28/service/GestionHabitacion.java
@@ -334,16 +334,13 @@ public class GestionHabitacion {
                 ocupantes.addAll(listaAcompanantes);
             }
 
-<<<<<<< HEAD
             //reserva.setAcompanantes(listaAcompanantes);
             reservaService.agregarAcompanantesAreserva(reserva.getIdReserva(), listaAcompanantes);
             reservaRepository.save(reserva);
-=======
             if (reserva != null) {
                 reserva.setAcompanantes(listaAcompanantes);
                 reservaRepository.save(reserva);
             }
->>>>>>> 4e2235b6f9d6f6f82a712daee70597fc44fb7846
 
             ocupantesAsignados.put(key, ocupantes);
         }
